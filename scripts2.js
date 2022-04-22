215b75fca0773454f9bac6a28200f6c61ed089f6
KelvinLow39 <103079218+KelvinLow39@users.noreply.github.com>
2022-04-22 17:58:15 +1000

    Create scripts2.js

---
+++ scripts2.js
@@ -0,0 +1,16 @@
+
+
+var url = "http://www.google.com";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
+
+
