6f8dfcfe9c8106d5e1345b88b527ba3d47da4997
KelvinLow39 <103079218+KelvinLow39@users.noreply.github.com>
2022-04-22 21:44:10 +1000

    N1

--- scripts2.js
+++ scripts2.js
@@ -1,16 +1,34 @@
 
 
-var url = "http://www.google.com";
-
+//var url = "http://www.google.com";
+var id1 =""
+var id2 = ""
+var url = "https://api.mypurecloud.com.au/api/v2/conversations/calls";
 var xhr = new XMLHttpRequest();
+
 xhr.open("GET", url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Authorization", "kCgnsHkurUJINtDP-v2tMeHaYIV_RVLIkHGBjcE_QNkCx3Ho-B_ooGJwEwfQ4WGjLoTdd7jYwCH4h2p1X8SRVA");
+
 
 xhr.onreadystatechange = function () {
    if (xhr.readyState === 4) {
+	  var obj = JSON.parse(xhr.responseText);
       console.log(xhr.status);
-      console.log(xhr.responseText);
+      //console.log(xhr.responseText);
+	  id1 = obj["entities"][0]["id"]
+	  id2 = obj["entities"][0]["participants"][3]["id"]
+  
    }};
+
+
+
+
+    document.write(obj["entities"][0]["id"]+ "<br>");
+    document.write(obj["entities"][0]["participants"][3]["id"] + "<br>");
+
 
 xhr.send();
 
 
+//GET -H "Content-Type: application/json" -H "Authorization: bearer kCgnsHkurUJINtDP-v2tMeHaYIV_RVLIkHGBjcE_QNkCx3Ho-B_ooGJwEwfQ4WGjLoTdd7jYwCH4h2p1X8SRVA" "https://api.mypurecloud.com.au/api/v2/conversations/calls"
