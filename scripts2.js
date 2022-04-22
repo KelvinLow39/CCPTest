098820221a74680f898ab5ad9946efd12ef6812d
KelvinLow39 <103079218+KelvinLow39@users.noreply.github.com>
2022-04-22 22:01:41 +1000

    1

--- scripts2.js
+++ scripts2.js
@@ -6,10 +6,10 @@ var id2 = ""
 var url = "https://api.mypurecloud.com.au/api/v2/conversations/calls";
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", url);
+
 xhr.setRequestHeader("Accept", "application/json");
 xhr.setRequestHeader("Authorization", "kCgnsHkurUJINtDP-v2tMeHaYIV_RVLIkHGBjcE_QNkCx3Ho-B_ooGJwEwfQ4WGjLoTdd7jYwCH4h2p1X8SRVA");
-
+xhr.open("GET", url);
 
 xhr.onload = function () {
    if (xhr.readyState === 4) {
